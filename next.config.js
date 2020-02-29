477ef570637ec408d1b303ff777b7a429c16a593
Mani <manigonzalez82@gmail.com>
2020-02-29 23:11:40 +0800

    Model user and signup user with json web token and cookies.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   env: {
     MONGO_SRV: "mongodb+srv://manigonzalez:halflife@cluster0-tu3ej.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "<insert-jwt-secret>",
+    JWT_SECRET: "skjdfhskdfhsdkjfbsddbf",
     CLOUDINARY_URL: "https://api.cloudinary.com/v1_1/manigonzalez/image/upload",
     STRIPE_SECRET_KEY: "<insert-stripe-secret-key>"
   }
